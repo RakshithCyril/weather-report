52922360fdbfd370f0d425cb0e5510379eeb691b
rakshith cyril <rakshithcyril@yahoo.com>
2021-10-18 18:43:34 +0530

    ready to try

---
+++ index.js
@@ -0,0 +1,13 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+
+app.get('/',(req,res)=>{
+    res.send('hesdfy')
+})
+app.listen(port,()=>{
+    console.log('listening')
+})
